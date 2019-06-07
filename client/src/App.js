607f59cd38dd27d09050bc38b669dde694895f40
Douglas Hill <dohill06@gmail.com>
2019-06-07 14:13:48 -0700

    Set up BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+
+
 import Header from './components/Header';
 import Courses from './components/Courses';
 
-function App() {
-    return (
+const App = () => (
+    <BrowserRouter>
         <div>
             <Header />
             <Courses />
         </div>
-    );
-}
+    </BrowserRouter>
+);
+
 
 export default App;
