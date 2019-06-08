c0b8e1611f15e69e24f1307571d579dd56ad1dc7
Douglas Hill <dohill06@gmail.com>
2019-06-08 14:19:44 -0700

    Add updatecourses route

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import CourseDetail from './components/CourseDetail';
 import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
 import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
 
 const App = () => (
     <BrowserRouter>
@@ -21,6 +22,7 @@ const App = () => (
             <Switch>
                 <Route exact path='/' component={Courses} />
                 <Route exact path='/courses/create' component={CreateCourse} />
+                <Route path='/courses/:id/update' component={UpdateCourse} />
                 <Route exact path='/courses/:id' component={CourseDetail} />
                 <Route path='/signin' component={UserSignIn} />
                 <Route path='/signup' component={UserSignUp} />
