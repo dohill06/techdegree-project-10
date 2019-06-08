54b4349b7472e45e606e291cbf6a9dcca1062d2e
Douglas Hill <dohill06@gmail.com>
2019-06-08 12:30:08 -0700

    Add createcourse route

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
+import CreateCourse from './components/CreateCourse';
 
 const App = () => (
     <BrowserRouter>
@@ -20,6 +21,7 @@ const App = () => (
             <Switch>
                 <Route exact path='/' component={Courses} />
                 <Route exact path='/courses/:id' component={CourseDetail} />
+                <Route path='/courses/create' component={CreateCourse} />
                 <Route path='/signin' component={UserSignIn} />
                 <Route path='/signup' component={UserSignUp} />
             </Switch>
