fe2dc20b1996efc6a60521af38c1cae3e64846a8
Douglas Hill <dohill06@gmail.com>
2019-06-08 11:31:54 -0700

    Update UserSignIn.js

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -10,13 +10,20 @@ class UserSignIn extends Component {
                     <h1>Sign In</h1>
                     <div>
                         <form>
-                            <div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" /></div>
-                            <div><input id="password" name="password" type="password" className="" placeholder="Password" value="" /></div>
-                            <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
+                            <div>
+                                <input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" />
+                            </div>
+                            <div>
+                                <input id="password" name="password" type="password" className="" placeholder="Password" value="" />
+                            </div>
+                            <div className="grid-100 pad-bottom">
+                                <button className="button" type="submit">Sign In</button>
+                                <Link className="button button-secondary" to='/'>Cancel</Link>
+                            </div>
                         </form> 
                     </div>
                     <p>&nbsp;</p>
-                    <p>Don't have a user account? <Link to="sign-up.html">Click here</Link> to sign up!</p>
+                    <p>Don't have a user account? <Link to='/signup'>Click here</Link> to sign up!</p>
                 </div>
             </div>
         )
