41d104fe08ca7d34f6a503b0141b5f84cacb5540
Douglas Hill <dohill06@gmail.com>
2019-06-07 18:40:44 -0700

    Make CourseDetail.js function

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,16 +1,39 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
 
 
 class CourseDetail extends Component {
+
+    state = {
+        course: {},
+        user: ''
+    };
+
+    componentDidMount() {
+        this.getCourse();
+    };
+
+    getCourse = () => {
+        axios.get(`http://localhost:5000/api${this.props.match.url}`)
+        .then(res => {
+            this.setState({
+                course: res.data.course,
+                user: `${res.data.course.User.firstName} ${res.data.course.User.lastName}`
+            });
+            console.log(res.data);
+            console.log(this.state);
+        })
+    };
 
     render() {
         return (
             <div>
                 <div className="actions--bar">
                     <div className="bounds">
-                        <div className="grid-100"><span><a className="button" href="update-course.html">Update Course</a><a className="button" href="#">Delete Course</a></span><a
-                        className="button button-secondary" href="index.html">Return to List</a>
+                        <div className="grid-100"><span><a className="button" href="update-course.html">Update Course</a><a className="button" href="delete-course.html">Delete Course</a></span>                        
+                        <Link className="button button-secondary" to='/'>Return to List</Link>
                         </div>
                     </div>
                 </div>
@@ -18,16 +41,11 @@ class CourseDetail extends Component {
                     <div className="grid-66">
                         <div className="course--header">
                             <h4 className="course--label">Course</h4>
-                            <h3 className="course--title">Build a Basic Bookcase</h3>
-                            <p>By Joe Smith</p>
+                            <h3 className="course--title">{this.state.course.title}</h3>
+                            <p>By {this.state.user}</p>
                         </div>
                         <div className="course--description">
-                            <p>High-end furniture projects are great to dream about. But unless you have a well-equipped shop and some serious woodworking experience to draw on, it can be difficult to turn the dream into a reality.</p>
-                            <p>Not every piece of furniture needs to be a museum showpiece, though. Often a simple design does the job just as well and the experience gained in completing it goes a long way toward making the next project even better.</p>
-                            <p>Our pine bookcase, for example, features simple construction and it's designed to be built with basic woodworking tools. Yet, the finished project is a worthy and useful addition to any room of the house. While it's meant to rest on the floor, you can convert the bookcase to a wall-mounted storage unit by leaving off the baseboard. You can secure the cabinet to the wall by screwing through the cabinet cleats into the wall studs.</p>
-                            <p>We made the case out of materials available at most building-supply dealers and lumberyards, including 1/2 x 3/4-in. parting strip, 1 x 2, 1 x 4 and 1 x 10 common pine and 1/4-in.-thick lauan plywood. Assembly is quick and easy with glue and nails, and when you're done with construction you have the option of a painted or clear finish.</p>
-                            <p>As for basic tools, you'll need a portable circular saw, hammer, block plane, combination square, tape measure, metal rule, two clamps, nail set and putty knife. Other supplies include glue, nails, sandpaper, wood filler and varnish or paint and shellac.</p>
-                            <p>The specifications that follow will produce a bookcase with overall dimensions of 10 3/4 in. deep x 34 in. wide x 48 in. tall. While the depth of the case is directly tied to the 1 x 10 stock, you can vary the height, width and shelf spacing to suit your needs. Keep in mind, though, that extending the width of the cabinet may require the addition of central shelf supports.</p>
+                            <ReactMarkdown source={this.state.course.description} />
                         </div>
                     </div>
                     <div className="grid-25 grid-right">
@@ -35,21 +53,12 @@ class CourseDetail extends Component {
                             <ul className="course--stats--list">
                                 <li className="course--stats--list--item">
                                     <h4>Estimated Time</h4>
-                                    <h3>14 hours</h3>
+                                    <h3>{this.state.course.estimatedTime}</h3>
                                 </li>
                                 <li className="course--stats--list--item">
                                     <h4>Materials Needed</h4>
                                     <ul>
-                                        <li>1/2 x 3/4 inch parting strip</li>
-                                        <li>1 x 2 common pine</li>
-                                        <li>1 x 4 common pine</li>
-                                        <li>1 x 10 common pine</li>
-                                        <li>1/4 inch thick lauan plywood</li>
-                                        <li>Finishing Nails</li>
-                                        <li>Sandpaper</li>
-                                        <li>Wood Glue</li>
-                                        <li>Wood Filler</li>
-                                        <li>Minwax Oil Based Polyurethane</li>
+                                        <ReactMarkdown source={this.state.course.materialsNeeded} />
                                     </ul>
                                 </li>
                             </ul>
