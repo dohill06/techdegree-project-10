4aeb03fa5f3e81218a47d459ad542bebf5f6a30a
Douglas Hill <dohill06@gmail.com>
2019-06-11 12:59:37 -0700

    Create Context.js

---
+++ client/src/components/Context.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+export const Context = React.createContext();
+
+export class Provider extends Component {
+    state = {
+        user: {}
+    }
+
+    signIn = (emailAddress, password) => {
+        axios.get('http://localhost:5000/api/users', {
+            auth: {
+                username: emailAddress,
+                password
+            }
+        }).then(res => {
+            console.log(res)
+        })
+    };
+
+    render() {
+        return (
+            <Context.Provider value{{
+                user: this.state.user,
+                actions: {
+                    signIn: this.signIn
+                }
+            }}>
+            {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+
+export const Consumer = Context.Consumer;
