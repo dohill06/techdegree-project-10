8c543b63261ad5bc7f6760261dff7033591fc620
Douglas Hill <dohill06@gmail.com>
2019-06-12 14:13:42 -0700

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Consumer } from './Context';
 
 const Header = () => {
     return (
-        <>
-        <div className="header">
-            <div className="bounds">
-                <h1 className="header--logo">Courses</h1>
-                <nav>
-                    <Link className="signup" to='/signup'>Sign Up</Link>
-                    <Link className="signin" to='/signin'>Sign In</Link>
-                </nav>
-            </div>
-        </div>
-        <hr />
-        </>
+        <Consumer>
+            {({ user }) => (
+                <>
+                <div className="header">
+                    <div className="bounds">
+                        <h1 className="header--logo">Courses</h1>
+                        <nav>
+                        {user.id ? (
+                            <>
+                            <span>{`Welcome ${user.firstName} ${user.lastName}`}</span>
+                            <Link className="signin" to='/signout'>Sign Out</Link>
+                            </>
+                        ) : (
+                            <>
+                            <Link className="signup" to='/signup'>Sign Up</Link>
+                            <Link className="signin" to='/signin'>Sign In</Link>
+                            </>
+                        )}
+                        </nav>
+                    </div>
+                </div>
+                <hr/>
+                </>
+            )}
+        </Consumer>
     )
 }
 
