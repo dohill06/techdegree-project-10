c99265d157d22086b40f590dba990f2827df3dbf
Douglas Hill <dohill06@gmail.com>
2019-06-15 10:13:23 -0700

    Set up localStorage to persist user data

--- client/src/components/Context.js
+++ client/src/components/Context.js
@@ -5,7 +5,7 @@ export const Context = React.createContext();
 
 export class Provider extends Component {
     state = {
-        user: {}
+        user: JSON.parse(localStorage.getItem('user'))
     };
 
     signIn = (emailAddress, password) => {
@@ -23,6 +23,10 @@ export class Provider extends Component {
             });
             console.log(this.state)
             console.log(res.status)
+            window.localStorage.setItem('user', JSON.stringify({
+                ...res.data,
+                password
+            }))
         })
     };
 
@@ -30,6 +34,7 @@ export class Provider extends Component {
         this.setState({
             user: {}
         });
+        window.localStorage.removeItem('user');
     };
 
     render() {
