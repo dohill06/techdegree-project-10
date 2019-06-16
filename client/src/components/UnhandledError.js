06cc7c1db5ac43d35f5ea0e4b4b4d2798dd52f70
Douglas Hill <dohill06@gmail.com>
2019-06-16 09:54:55 -0700

    Update UnhandledError.js

--- client/src/components/UnhandledError.js
+++ client/src/components/UnhandledError.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UnhandledError = () => {
     return (
         <div className='bounds'>
             <h1>500 Internal Server Error</h1>
             <h2>There seems to be a problem</h2>
+            <p>&nbsp;</p>
+            <Link className="button button-secondary" to='/'>Return to List</Link>
         </div>
     )
 }
