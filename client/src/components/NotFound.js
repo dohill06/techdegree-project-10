02c68ef6d7f16ccd31b7ae763366d9f553bcc643
Douglas Hill <dohill06@gmail.com>
2019-06-16 09:52:29 -0700

    Update NotFound.js

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div className='bounds'>
             <h1>404 Not Found</h1>
             <h2>The page you are looking for could not be found</h2>
+            <p>&nbsp;</p>
+            <Link className="button button-secondary" to='/'>Return to List</Link>
         </div>
     )
 }
